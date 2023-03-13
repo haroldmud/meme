acd6ffc6c056356214fff593a030f0d8ae3f68c7
Harold Mudosa <91553966+haroldmud@users.noreply.github.com>
2023-03-13 17:30:56 +0200

    Update components/header.jsx
    Co-authored-by: Samantha Murera Kayitete <86477268+Samiskina@users.noreply.github.com>

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,6 @@
 export default function Header(){
   return(
-    <header className="flex justify-center h-[65px]  bg-[#8924ad] text-white">
+    <header className="flex justify-center h-16  bg-fuchsia-700 text-white">
             <div className="flex md:w-[95%] w-[80%] justify-between">
               <div className="flex gap-[6.69px]">
                 <div className=" flex flex-col justify-center w-[41px]">
