b71047ff6edaf897ccde531af77417acf1195664
haroldmud <haroldmudosa@gmail.com>
2023-03-13 18:38:49 +0200

    Feat: Delete the commented lines

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   let x = 3;
-  // const images = data;
   const [random, setRandom] = useState("https://i.imgflip.com/30b1gx.jpg");
   const [first,setFirst]=useState("");
   const [second,setSecond]=useState("");
@@ -55,34 +54,3 @@ export default function Home() {
     </section>
   )
 }
-
-
-// export default function Things(){
-//   const [item,setItem] = useState([]);
-//   const [input,setInput] = useState("")
-//   return(
-//     <div className="flex justify-center font-bold mt-4">
-//       <div>
-//         <form action="">
-//         <div className="flex justify-center">
-//         <textarea rows="4" cols="50" className="border pl-2" type="text" value={input} onChange={(e)=>setInput(e.target.value)} />
-//         </div>
-//         <div className="flex justify-center mt-2">
-//         <button  onClick={(e)=>{e.preventDefault(); setItem((current)=>[...current,`${input}`]); setInput("")}} className="border-2 bg-purple-500 rounded-xl px-8 py-2">Add new item +</button>
-//        </div>
-
-//         </form>
-//         <div className="flex justify-center">
-//         <ul>
-//           {
-//             item.map((x,y)=>
-//             <li key={y}>{x}</li>
-//             )
-//           }
-//         </ul>
-//         </div>
-//       </div>
-
-//     </div>
-//   )
-// }
